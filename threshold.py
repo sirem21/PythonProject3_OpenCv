149b1b1b5ce9eb2402cb720b15a231a059feac71
User <saharirem03@gmail.com>
2025-06-14 14:02:24 +0300

    threshold functions are written

--- threshold.py
+++ threshold.py
@@ -1,6 +1,15 @@
 import cv2 as cv
 
-img = cv.imread("img1.jpg")
+img = cv.imread("1702829106037.jpg")
 cv.imshow("orig", img)
 
-threshold, thresh = cv.threshold(img, 150, 255, cv.THRESH_BINARY)
+grayscale = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+# Simple Threshold
+threshold, thresh = cv.threshold(grayscale, 150, 255, cv.THRESH_BINARY)
+cv.imshow("simple", thresh)
+# Adaptive Threshold
+adapt_threshold = cv.adaptiveThreshold(grayscale,255,cv.ADAPTIVE_THRESH_MEAN_C,cv.THRESH_BINARY,11,1 )
+cv.imshow("Adaptive", adapt_threshold)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
